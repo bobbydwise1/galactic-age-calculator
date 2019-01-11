d81f5519c15a72b8d03fb9d147b694b07583f34b
Rohan Moore <rohanpmoore@gmail.com>
2019-01-11 09:41:34 -0800

    refactor specs to include a beforeEach

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,24 +1,22 @@
 import { Calculator } from "./../src/calculator.js"
 
 describe('Calculator', function() {
-  it("should take the user's birthday and store the appropriate Date object and other relevant information.", function() {
+  beforeEach(function() {
     const calculator = new Calculator({age: 21});
+  });
+  it("should take the user's age and store the information.", function() {
     expect(calculator.age).toEqual(21);
   });
   it("should calculate the number of years a person has lived in Mecury years.", function() {
-    const calculator = new Calculator({age: 21});
     expect(calculator.mercuryYears()).toEqual(87);
   });
   it("should calculate the number of years a person has lived in Venus years.", function() {
-    const calculator = new Calculator({age: 21});
     expect(calculator.venusYears()).toEqual(33);
   });
   it("should calculate the number of years a person has lived in Mars years.", function() {
-    const calculator = new Calculator({age: 21});
     expect(calculator.marsYears()).toEqual(11);
   });
   it("should calculate the number of years a person has lived in Jupiter years.", function() {
-    const calculator = new Calculator({age: 21});
     expect(calculator.jupiterYears()).toEqual(1);
   });
 });
