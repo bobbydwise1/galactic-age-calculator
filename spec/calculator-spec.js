e8016ab60bc6fad9f83ec86abcaa9bdf2f5739bb
Rohan Moore <rohanpmoore@gmail.com>
2019-01-11 08:29:31 -0800

    add spec one, failed

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,5 +1,10 @@
 import { Calculator } from "./../src/calculator.js"
 
 describe('Calculator', function() {
-  // it("should take the user's birthday and ")
+  it("should take the user's birthday and store the appropriate Date object and other relevant information", function() {
+    let calculator = new Calculator({day: 31, month: 2, year: 1997});
+    expect(calculator.birthday.getDate()).toEqual(31);
+    expect(calculator.birthday.getMonth()).toEqual(2);
+    expect(calculator.birthday.getFullYear()).toEqual(1997);
+  });
 });
