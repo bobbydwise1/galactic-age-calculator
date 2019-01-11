8a7f82e47e1eafbc6c9cad98bd8e4a72e8f48da8
Rohan Moore <rohanpmoore@gmail.com>
2019-01-11 10:00:15 -0800

    refactored code to call the same function, with different planetary values being part of an array

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -8,15 +8,15 @@ describe('Calculator', function() {
     expect(calculator.age).toEqual(21);
   });
   it("should calculate the number of years a person has lived in Mecury years.", function() {
-    expect(calculator.mercuryYears()).toEqual(87.5);
+    expect(calculator.planetaryYears(0)).toEqual(87.5);
   });
   it("should calculate the number of years a person has lived in Venus years.", function() {
-    expect(calculator.venusYears()).toEqual(33.87);
+    expect(calculator.planetaryYears(1)).toEqual(33.87);
   });
   it("should calculate the number of years a person has lived in Mars years.", function() {
-    expect(calculator.marsYears()).toEqual(11.17);
+    expect(calculator.planetaryYears(2)).toEqual(11.17);
   });
   it("should calculate the number of years a person has lived in Jupiter years.", function() {
-    expect(calculator.jupiterYears()).toEqual(1.77);
+    expect(calculator.planetaryYears(3)).toEqual(1.77);
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,12 @@
+const planetValues = [.24, .62, 1.88, 11.86}];
+
 export class Calculator {
   constructor(params) {
     this.age = params.age;
   }
 
-  mercuryYears() {
-    return (this.age/.24).toFixed(2);
-  }
-
-  venusYears() {
-    return (this.age/.62).toFixed(2);
-  }
-
-  marsYears() {
-    return (this.age/1.88).toFixed(2);
-  }
-
-  jupiterYears() {
-    return (this.age/11.86).toFixed(2);
+  planetaryYears(planet) {
+    return (this.age/planetValues[planet]).toFixed(2);
   }
 }
 
