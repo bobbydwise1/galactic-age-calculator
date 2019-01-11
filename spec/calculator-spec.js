7e8517c1aedd8e8e947bc3dee45a9baa3bf18d77
Rohan Moore <rohanpmoore@gmail.com>
2019-01-11 09:17:39 -0800

    change function to take years instead of birthday and create spec two

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,10 +1,12 @@
 import { Calculator } from "./../src/calculator.js"
 
 describe('Calculator', function() {
-  it("should take the user's birthday and store the appropriate Date object and other relevant information", function() {
-    let calculator = new Calculator({day: 31, month: 2, year: 1997});
-    expect(calculator.birthday.getDate()).toEqual(31);
-    expect(calculator.birthday.getMonth()).toEqual(2);
-    expect(calculator.birthday.getFullYear()).toEqual(1997);
+  it("should take the user's birthday and store the appropriate Date object and other relevant information.", function() {
+    const calculator = new Calculator({age: 21});
+    expect(calculator.age).toEqual(21);
+  });
+  it("should calculate the number of years a person has lived in Mecury years.", function() {
+    const calculator = new Calculator({age: 21});
+    expect(calculator.mercuryYears()).toEqual(87);
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,5 @@
 export class Calculator {
   constructor(params) {
-    let day = params.day;
-    let month = params.month;
-    let year = params.year;
-    this.birthday = new Date(year, month, day);
+    this.age = params.age;
   }
 }
