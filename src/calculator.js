7dce63bb923bdf1900bb6cb44f58da2a4fef3ed6
Rohan Moore <rohanpmoore@gmail.com>
2019-01-11 08:32:51 -0800

    complete spec one

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,8 @@
 export class Calculator {
   constructor(params) {
-    
+    let day = params.day;
+    let month = params.month;
+    let year = params.year;
+    this.birthday = new Date(year, month, day);
   }
 }
